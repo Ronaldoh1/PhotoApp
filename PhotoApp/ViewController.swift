702e58450122873422a0195ee75dc4b54aaf56ca
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-02-09 14:38:29 -0500

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by Hernandez, Ronald on 2/9/20.
+//  Copyright © 2020 Ronaldoh1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
