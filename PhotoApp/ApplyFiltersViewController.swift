4b83fa13471ebdaa81385c6b6b518e02b063c369
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-02-10 05:01:19 -0500

    set up UI

---
+++ PhotoApp/ApplyFiltersViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ApplyFiltersViewController.swift
+//  PhotoApp
+//
+//  Created by Hernandez, Ronald on 2/10/20.
+//  Copyright © 2020 Ronaldoh1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ApplyFiltersViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+
+    }
+}
---
+++ PhotoApp/PhotoSelectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoSelectionViewController.swift
+//  PhotoApp
+//
+//  Created by Hernandez, Ronald on 2/10/20.
+//  Copyright © 2020 Ronaldoh1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoSelectionViewController: UICollectionViewController {
+    
+    
+}
