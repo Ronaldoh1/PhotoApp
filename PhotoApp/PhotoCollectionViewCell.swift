736dcff3775229eef26e8d4e1710bda380c4e723
Ronaldoh1 <2ronald.hernandez@gmail.com>
2020-02-14 13:18:15 -0500

    added photo collection view cell

---
+++ PhotoApp/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoApp
+//
+//  Created by Hernandez, Ronald on 2/14/20.
+//  Copyright © 2020 Ronaldoh1. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImage: UIView!
+    
+    
+}
--- PhotoApp/PhotoSelectionViewController.swift
+++ PhotoApp/PhotoSelectionViewController.swift
@@ -8,8 +8,45 @@
 
 import Foundation
 import UIKit
+import Photos
 
 class PhotoSelectionViewController: UICollectionViewController {
     
+    private var images = [PHAsset]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pupulatePhotos()
+    }
+    
+    private func pupulatePhotos() {
+        PHPhotoLibrary.requestAuthorization { [weak self] status in
+            if status == .authorized {
+                // access the pictures of photos.
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects { (object, count, stop) in
+                    self?.images.append(object)
+                }
+                self?.images.reverse() // need to add the images in reverse order because of the order they were added in.
+                print(self?.images)
+            }
+        }
+    }
+    
+}
+
+extension PhotoSelectionViewController {
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
 }
